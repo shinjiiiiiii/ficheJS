98765e07472a3079c38fe53f8e879b98d7e08711
shinjiiiiiii <udauvergne@edenschool.fr>
2022-09-23 14:00:28 +0200

    type de variable et operations sur les variables

--- js/app.js
+++ js/app.js
@@ -6,4 +6,26 @@ age=15;
 console.log(typeof age);
 const prenom="ugo";
 //prenom est de type string 
+let addition = 4+12;
+console.log(addition);
+let soustraction =2-50; 
+console.log(soustraction);
+let multiplication=3*8;
+console.log(multiplication);
+let division=7/2;
+console.log(division);
+let modulo=5%2;
+console.log(modulo);
+let test=3>5;
+console.log(test);
+console.log(typeof test);
+//test est une variable de type boolean 
+//=== == != !== <= >= < > 
+const identity = "Bonjour" + " " + prenom + " " + "vous avez" + " " + age + " " + "ans"
+//c'est une addition
 
+
+let masjuscule=identity.toUpperCase();
+console.log(identity);
+console.log(masjuscule);
+//j'ai enregistrer identity en maj dans majusucle car je ne peux pas modifier une constante 
